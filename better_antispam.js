792e16120a9e5385f6f7260d3f0dc1631117ebee
SonMaxime <47661880+SonMaxime@users.noreply.github.com>
2020-04-28 09:50:18 +0200

    French Translation

--- better_antispam.js
+++ better_antispam.js
@@ -1,4 +1,4 @@
-const { MessageEmbed } = require("discord.js"); // Requiring this since we need it for embeds later
+const { MessageEmbed } = require("discord.js");
 
 let authors = [];
 let warned = [];
@@ -6,38 +6,33 @@ let punishedList = [];
 let messageLog = [];
 
 module.exports = async (client, options) => {
-  /* Declaring our options which we are going to work on */
   
-  const limitUntilWarn = (options && options.limitUntilWarn) || 3; // Default value: 3. Explication: This is the limit where you get the warn message. If the member X sent over 3 messages within the interval, he get warned
-  const limitUntilMuted = (options && options.limitUntilMuted) || 5; // Default value: 5. Explication: This is the limit where you get Punished. If the member X sent over 5 messages within the interval, he get muted.
-  const interval = (options && options.interval) || 2000; //Default Time: 2000MS (1000 milliseconds = 1 second, 2000 milliseconds = 2 seconds etc...). Explication: The interval where the messages are sent. Practically if member X sent 5+ messages within 2 seconds, he get muted
-  const warningMessage = (options && options.warningMessage) || "if you don't stop from spamming, I'm going to punish you!"; // Default Message: if you don't stop from spamming, I'm going to punish you!. Explication: None, it's just a message you get for the warning phase.
-  const muteMessage = (options && options.muteMessage) || "was muted since we don't like too much advertisement type people!"; // Default Message: "was muted since we don't like too much advertisement type people!". Explication: The message sent after member X was punished
-  const maxDuplicatesWarning = (options && options.maxDuplicatesWarning || 7); // Default value: 7. Explication: When people are spamming the same message, <limitUntilWarn> is ignored and this will trigger when member X sent over 7+ message that are the same.
-  const maxDuplicatesMute = (options && options. maxDuplicatesMute || 10); // Deafult value: 10 Explication: The limit where member X get muted after sending too many messages(10+).
-  const ignoredRoles = (options && options.ignoredRoles) || []; // Default value: None. Explication: The members with this role(or roles) will be ignored if they have it. Suggest to not add this to any random guys.
-  const ignoredMembers = (options && options.ignoredMembers) || []; // Default value: None. Explication: These members are directly affected and they do not require to have the role above. Good for undercover pranks.
-  const mutedRole = (options && options.mutedRole) || "muted"; // Default value: muted. Explication: Here you put the name of the role that should not let people write/speak or anything else in your server. If there is no role set, by default, the module will attempt to create the role for you & set it correctly for every channel in your server. It will be named "muted".
-  const timeMuted = (options && options.timeMuted) || 1000 * 600; // Default value: 10 minutes. Explication: This is how much time member X will be muted. if not set, default would be 10 min.
-  const logChannel = (options && options.logChannel) || "AntiSpam-logs"; // Default value: "AhtiSpam-logs". Explication: This is the channel where every report about spamming goes to. If it's not set up, it will attempt to create the channel.
+  const limitUntilWarn = (options && options.limitUntilWarn) || 3;
+  const limitUntilMuted = (options && options.limitUntilMuted) || 5;
+  const interval = (options && options.interval) || 2000;
+  const warningMessage = (options && options.warningMessage) || "Si vous n’arrêtez pas de spammer, je vais vous punir !"; 
+  const muteMessage = (options && options.muteMessage) || "a été mis en sourdine car nous n’aimons pas trop les gens de type spammeur...";
+  const maxDuplicatesWarning = (options && options.maxDuplicatesWarning || 7);
+  const maxDuplicatesMute = (options && options. maxDuplicatesMute || 10);
+  const ignoredRoles = (options && options.ignoredRoles) || [];
+  const ignoredMembers = (options && options.ignoredMembers) || [];
+  const mutedRole = (options && options.mutedRole) || "Muté"; 
+  const timeMuted = (options && options.timeMuted) || 1000 * 600;
+  const logChannel = (options && options.logChannel) || "reports";
 
-// If something is added wrong, throw an error
-
-  if(isNaN(limitUntilWarn)) throw new Error("ERROR: <limitUntilWarn> option is not set up right! Please check it again to be a number in settings.");
-  if(isNaN(limitUntilMuted)) throw new Error("ERROR: <limitUntilMuted> option is not set up right! Please add a number in settings.");
-  if(isNaN(interval)) throw new Error("ERROR: <interval> option is not set up right! Please add a number in settings.");
-  if(!isNaN(warningMessage) || warningMessage.length < 5) throw new Error("ERROR: <warningMessage> option must be a string and have at least 5 characters long (Including space).");
-  if(!isNaN(muteMessage) || muteMessage.length < 5) throw new Error("ERROR: <muteMessage> option must be a string and have at least 5 characters long (Including space).");
-  if(isNaN(maxDuplicatesWarning)) throw new Error("ERROR: <maxDuplicatesWarning> option is not set up right! Please check it again to be a number in settings.")
-  if(isNaN(maxDuplicatesMute)) throw new Error("ERROR: <maxDuplicatesMute> option is not set up right! Please check it again to be a number in settings.");
-  if(isNaN(timeMuted)) throw new Error("ERROR: <timeMuted> option is not set up right! Please check it again to be a number in settings.");
-  if(ignoredRoles.constructor !== Array) throw new Error("ERROR: <ignoredRoles> option is not set up right! Please check it again to be an array in settings.");
-  if(ignoredMembers.constructor !== Array) throw new Error("ERROR: <ignoredMembers> option is not set up right! Please check it again to be an array in settings.");
+  if(isNaN(limitUntilWarn)) throw new Error("ERREUR : L’option <limitUntilWarn> n’est pas configurée correctement ! Veuillez vérifier à nouveau qu’il s’agit d’un nombre dans les paramètres.");
+  if(isNaN(limitUntilMuted)) throw new Error("ERREUR : L’option <limitUntilMuted> n’est pas configurée correctement ! Veuillez ajouter un nombre dans les paramètres.");
+  if(isNaN(interval)) throw new Error("ERREUR : L’option <interval> n’est pas configurée correctement ! Veuillez ajouter un nombre dans les paramètres.");
+  if(!isNaN(warningMessage) || warningMessage.length < 5) throw new Error("ERREUR : L’option <warningMessage> doit être une chaîne de caractères et comporter au moins 5 caractères (y compris l’espace).");
+  if(!isNaN(muteMessage) || muteMessage.length < 5) throw new Error("ERREUR : L’option <muteMessage> doit être une chaîne de caractères et avoir au moins 5 caractères (y compris l’espace).");
+  if(isNaN(maxDuplicatesWarning)) throw new Error("ERREUR : L’option <maxDuplicatesWarning> n’est pas configurée correctement ! Veuillez vérifier à nouveau qu’il s’agit d’un nombre dans les paramètres.")
+  if(isNaN(maxDuplicatesMute)) throw new Error("ERREUR : L’option <maxDuplicatesMute> n’est pas configurée correctement ! Veuillez vérifier à nouveau qu’il s’agit d’un nombre dans les paramètres.");
+  if(isNaN(timeMuted)) throw new Error("ERREUR : L’option <timeMuted> n’est pas configurée correctement ! Veuillez vérifier à nouveau qu’il s’agit d’un nombre dans les paramètres.");
+  if(ignoredRoles.constructor !== Array) throw new Error("ERREUR : L’option <ignoredRoles> n’est pas configurée correctement ! Veuillez vérifier à nouveau qu’il s’agit d’un tableau dans les paramètres.");
+  if(ignoredMembers.constructor !== Array) throw new Error("ERREUR : L’option <ignoredMembers> n’est pas configurée correctement ! Veuillez vérifier à nouveau qu’il s’agit d’un tableau dans les paramètres.");
   
-  // Custom 'checkMessage' event that handles messages
  client.on("checkMessage", async (message) => {
  
-  //time variables
   let clock = new Date();
   let ss = String(clock.getSeconds()).padStart(2, '0');
   let min = String(clock.getMinutes()).padStart(2, '0');
@@ -45,15 +40,13 @@ module.exports = async (client, options) => {
   clock = hrs + ':' + min +':' + ss;
 
   let TheDate = new Date()
-  let zilelesaptamanii = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+  let zilelesaptamanii = ["Dimanche", "Lundi", "Mardi", "Mercredi", "Jeudi", "Vendredi", "Samedi"];
   let weekday = zilelesaptamanii[TheDate.getDay()];
   let dd = String(TheDate.getDate()).padStart(2, '0');
   let mon = String(TheDate.getMonth()+ 1);
   let year = String(TheDate.getFullYear()).padStart(4,'00');
   TheDate = weekday+", " + mon + '/' + dd +'/' + year;
-  //end of time variables
 
-  //verify if it's pm or AM
   let amORpm;
   if(hrs >= 0 && hrs <= 12){
       amORpm = "AM"
@@ -81,24 +74,24 @@ module.exports = async (client, options) => {
                 deny: ['VIEW_CHANNEL']
               }]
             })
-              .then(m=> m.send(`Created **\`anti-spam-Logs\`** channel since a channel for reports wasn't provided from beginning when setting up the module.`))
+              .then(m=> m.send(`Création du canal ** "reports"** puisqu’un canal pour les rapports n’a pas été fourni dès le début lors de la configuration du module.`))
               .catch(console.error)
   
         }catch(e){
           console.log(e.stack);
         }
-      }; // end of creating the channel for anti spam logs
+      };
 
       let role = m.guild.roles.cache.find(namae => namae.name === mutedRole);      
       if (!role) {
         try {
             role = await m.guild.roles.create({
               data:{
-                name: "muted",
+                name: "Muté",
                 color: "#000000",
                 permissions: []
               },
-              reason: `muted role wasn't found! Created a new one!`
+              reason: `Role non trouvé, merci d'en créer un.`
             })
             m.guild.channels.cache.forEach(async (thechann, id) => {
                 await thechann.updateOverwrite(role, {
@@ -109,42 +102,41 @@ module.exports = async (client, options) => {
                     SPEAK: false
                 });
             });
-           ReportChannel.send(`Created **\`muted\`** role since a role(to be applied to muted person) wasn't provided from beginning when setting up the module.`) 
+           ReportChannel.send(`Création du rôle ** "Muté"** puisqu’un rôle (à appliquer à la personne muette) car n’a pas été fourni dès le début lors de la configuration du module.`) 
         } catch (e) {
             console.log(e.stack);
         }
-    }//end of creating the role
+    }
     
       if (user) {
         user.roles.add(role).then(()=>{
           m.channel.send(`<@!${m.author.id}>, ${muteMsg}`);
           let muteEmbed = new MessageEmbed()
-            .setAuthor(' Action | Auto Mute', `https://images-ext-2.discordapp.net/external/Wms63jAyNOxNHtfUpS1EpRAQer2UT0nOsFaWlnDdR3M/https/image.flaticon.com/icons/png/128/148/148757.png`)
-            .addField('Member muted:',`${user}`)
-            .addField(`How much time got muted?:`,`${timeMuted} seconds (10 min)`)
-            .addField('Reason of mute: ', `Spam`)
-            .addField(`When it was muted that person:`,TheDate+ " at "+ clock+" "+amORpm)
+            .setAuthor('Action | Auto Mute', `https://cdn.discordapp.com/icons/690549564620537889/33d5bb0874db59d78a5817d3964ee7e2.jpg`)
+            .addField('Membre muté:',`${user}`)
+            .addField(`Muté combien de temps ? :`,`${timeMuted} seconds (10 min)`)
+            .addField('Raison du mute : ', `Spam`)
+            .addField(`Quand a-t-il été mute ? :`,TheDate+ " at "+ clock+" "+amORpm)
             .setColor('#D9D900')
           ReportChannel.send(muteEmbed);
           setTimeout(()=>{
             user.roles.remove(role);
             let unmutedEmbed = new MessageEmbed()
               .setAuthor('Action | Auto Unmute')
-              .addField(`Member unmuted:`,`${user}`)
-              .addField(`Reason of unmute:`,`Time Expired(10 min)`)
+              .addField(`Membre unmute:`,`${user}`)
+              .addField(`Raison de l'unmute:`,`Temps atteint (10 min)`)
               .setColor('#D9D900')
           ReportChannel.send(unmutedEmbed)
           }, timeMuted);
           return true;
        }).catch((e) => {
-          m.guild.owner.send(`Oops, seems like i don't have sufficient permissions to mute <@!${message.author.id}>!\n It can be that or another type of error happened! Tell me on github: https://github.com/MirageZoe/ \n Everything happened on ${TheDate} at ${clock} ${amORpm} with message:\n\n\`${e.message}\`\n\n *P.S: If this is the first time getting something like this, most likely because it was not set up good the log channel at beginning and didn't know where to send the reports. Do not panic, next time it will work since he created the channel where to send the reports!*`);
+          m.guild.owner.send(`Oops je n'ai pas les permissions... <@!${message.author.id}>!\n Il se peut que ce soit un autre type d’erreur qui s’est produitt ! Tell me on github: https://github.com/MirageZoe/ \n Tout c'est passé le ${TheDate} à ${clock} ${amORpm} avec le message:\n\n\`${e.message}\`\n\n *P.S: Si c’est la première fois que vous obtenez quelque chose comme ça, très probablement parce qu’il n’a pas été bien configuré le canal journal au début et ne savait pas où envoyer les rapports. Ne paniquez pas, la prochaine fois ça va marcher puisqu’il a créé le canal où envoyer les rapports !*`);
           return false;
       });
-    }//end of user
+    }
   }
   
     
-   // The warning function.
    const WarnMember = async (m, reply) => {
     warned.push(m.author.id);
     m.channel.send(`<@${m.author.id}>, ${reply}`);
@@ -158,13 +150,13 @@ module.exports = async (client, options) => {
     if (message.author.id !== client.user.id) {
       let currentTime = Math.floor(Date.now());
       authors.push({
-        "time": currentTime,
-        "author": message.author.id
+        "temps": currentTime,
+        "auteur": message.author.id
       });
       
       messageLog.push({
         "message": message.content,
-        "author": message.author.id
+        "auteur": message.author.id
       });
       
       let msgMatch = 0;
